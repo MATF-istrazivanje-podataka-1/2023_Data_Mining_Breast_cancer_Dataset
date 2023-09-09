a3a426dcc913afeefd3cac61753e0a870bc7d3a6
Brankonymous <brankogrbic01@gmail.com>
2023-09-10 01:13:44 +0200

    add knn and fix xgboost

--- preprocessing/extract_datasets.py
+++ preprocessing/extract_datasets.py
@@ -11,49 +11,54 @@ class DatasetExtractor():
         self.df = pd.read_csv(file_path, index_col=0)
 
     def datasets_exists(self, pca_components):
-        return os.path.exists(self.additional_datasets_path + self.dataset_name + '_normalized.csv') and \
-            os.path.exists(self.additional_datasets_path + self.dataset_name + f'_pca_{pca_components}.csv')
+        return os.path.exists(self.additional_datasets_path + self.dataset_name + '_norm.csv') and \
+            os.path.exists(self.additional_datasets_path + self.dataset_name + f'_pca.csv')
 
-    def extract_datasets(self, pca_components=10):
-        if self.datasets_exists(pca_components):
-            print('Datasets already exists')
-            return
-
+    def split_X_y(self):
         X = self.df.drop(columns=['type'])
-
         y = self.df.type
         unique_classes = y.unique()
         class_mapping = {label: idx for idx, label in enumerate(unique_classes)}
         y = y.map(class_mapping)
 
-        self.extract_normalized_dataset(X, y)
-        self.extract_pca_dataset(X, y, pca_components, name=f'{self.dataset_name}_pca')
+        return X, y
+
+    def extract_datasets(self, pca_components=10):
+        self.extract_normalized_dataset()
+        self.extract_pca_dataset(pca_components, name=f'{self.dataset_name}_pca')
     
-    def extract_normalized_dataset(self, X, y, name=dataset_name + '_norm'):
+    def extract_normalized_dataset(self, name=dataset_name + '_norm'):
+        X, y = self.split_X_y()
         X_normalized = (X - X.mean()) / X.std()
-        df_norm = pd.concat([X_normalized, y], axis=1)
+        df_norm = X_normalized.copy()
+        df_norm['type'] = y
         
         isExist = os.path.exists(self.additional_datasets_path)
         if not isExist:
             os.makedirs(self.additional_datasets_path)
         
-        df_norm.to_csv(self.additional_datasets_path+name+'.csv', index=False)
+        df_norm.to_csv(self.additional_datasets_path+name+'.csv')
 
-    def extract_pca_dataset(self, X, y, pca_components, name=f'Breast_GSE45827_pca'):
+    def extract_pca_dataset(self, pca_components, name=f'Breast_GSE45827_pca'):
+        X, y = self.split_X_y()
         X_normalized = (X - X.mean()) / X.std()
         pca = PCA(n_components = pca_components)
         pca.fit(X_normalized)
-        X_pca = pd.DataFrame(pca.transform(X_normalized))
 
-        X_pca['type'] = y
+        df_pca = pd.DataFrame(pca.transform(X_normalized), columns=[f'PC{i}' for i in range(1, pca_components+1)], index=X_normalized.index)
+        df_pca['type'] = y
 
-        X_pca.to_csv(self.additional_datasets_path+name+'.csv', index=False)
+        isExist = os.path.exists(self.additional_datasets_path)
+        if not isExist:
+            os.makedirs(self.additional_datasets_path)
+        
+        df_pca.to_csv(self.additional_datasets_path+name+'.csv')
 
 
 if __name__ == "__main__":
-    PCA_COMPONENTS = 10
+    PCA_COMPONENTS = 80
     dataset_extractor = DatasetExtractor()
 
-    dataset_extractor.extract_datasets(pca_components=PCA_COMPONENTS)
+    dataset_extractor.extract_pca_dataset(pca_components=PCA_COMPONENTS)
 
     print('Done')
